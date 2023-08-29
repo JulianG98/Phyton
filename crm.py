856b8953f4f417255ccdf3d2fc5a5233cc1f2c69
JulianG98 <julian.greber@gmail.com>
2023-08-29 18:51:07 +0200

    test 3

--- crm.py
+++ crm.py
@@ -6,22 +6,23 @@ from openpyxl.chart import Reference, Series
 # Schreibe eine Funktion die neuen Kunde mit allen Wert ins CRM aufnimmt. Werte sollen über den Argparse hinzugefügt werden können. Und füge min. 10 Kunden mit verschiedenen Werten über diesen Aufruf hinzu in die Excel. 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("-N", "--Name", dest="crm.xlsx", help="wie lautet der Name von dem Kunden", required=True)
-parser.add_argument("-E", "--Email", dest="crm.xlsx", help="wie lauftet die email von dem Kunden", required=True)
-parser.add_argument("-S", "--Status", dest="crm.xlsx", help="ist der verkauf abgeschlossen oder nicht", required=True)
-parser.add_argument("-Sg", "--Stage", dest="crm.xlsx", help="in welcher Phase befinden wir uns", required=True)
-parser.add_argument("-W", "--Wählversuche", dest="crm.xlsx", help="wie oft hat der verkäufer die Nummer gewählt", required=True)
-parser.add_argument("-P", "--Produktverkaufspreis", dest="crm.xlsx", help="für wie viel wurde das Produkt verkauft", required=True)
-parser.add_argument("-Z", "--Zeit bis Sale", dest="crm.xlsx", help="Wie viele Tage bis zum verkauf", required=True)
-parser.add_argument("-V", "--Verkäufer", dest="crm.xlsx", help="Welcher Verkäufer hat den verkauf abgeschlossen", required=True)
+parser.add_argument("-N", "--Name", dest="Name", help="wie lautet der Name von dem Kunden", required=True)
+parser.add_argument("-E", "--Email", dest="Email", help="wie lauftet die email von dem Kunden", required=True)
+parser.add_argument("-S", "--Status", dest="Status", help="ist der verkauf abgeschlossen oder nicht", required=True)
+parser.add_argument("-Sg", "--Stage", dest="Stage", help="in welcher Phase befinden wir uns", required=True)
+parser.add_argument("-W", "--Wählversuche", dest="Wählversuche", help="wie oft hat der verkäufer die Nummer gewählt", required=True)
+parser.add_argument("-P", "--Produktverkaufspreis", dest="Produktverkaufpreis", help="für wie viel wurde das Produkt verkauft", required=True)
+parser.add_argument("-Z", "--Zeit", dest="Zeit", help="Wie viele Tage bis zum verkauf", required=True)
+parser.add_argument("-V", "--Verkäufer", dest="Verkäufer", help="Welcher Verkäufer hat den verkauf abgeschlossen", required=True)
 
 args = parser.parse_args()
-f = open(args.crm.xlsx, "r")
-print("Alles: " + f.read())
+#f = open(args.Name, "r")
+#print("Alles: " + f.read())
 
 wb= openpyxl.load_workbook("crm.xlsx", data_only= True)
 Worksheet = wb.active
-Worksheet["A5"] = "Raja König"
+Worksheet.append([args.Name, args.Email, args.Status, args.Stage, args.Wählversuche, args.Produktverkaufpreis, args.Zeit , args.Verkäufer])
+wb.save("crm.xlsx")
 # Schreibe eine Funktion die bestehende Kundeneinträge im CRM aktualisiert. Werte sollen über den Argparse hinzugefügt werden können
 
 
